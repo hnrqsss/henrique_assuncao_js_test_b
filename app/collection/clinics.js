353d52aa44b84da173c3a9af16d2e93aaf34082d
hnrqss <hnrq.assuncao@gmail.com>
2018-12-12 18:17:11 -0300

    navigate into clinic routes

--- app/collection/clinics.js
+++ app/collection/clinics.js
@@ -3,11 +3,10 @@ const Clinics = Backbone.Collection.extend({
     model: Clinic,
     url: 'http://localhost:3001/clinics',
 
-    initialize: function() {
-        
-    },
-
     navigateToClinic: function(id) {
-        console.log(id)
+        
+        const router = new Router()
+
+        router.navigate(`#clinic/${id}`,true)
     }
 })
